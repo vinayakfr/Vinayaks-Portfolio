e088908af0212ae3d38596d0ca885e19d279f8b1
vinayakfr <vinayak.sri@yahoo.com>
2024-03-24 18:41:12 +0530

    feat: add contact page

---
+++ app/routes/contact.tsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/navbar";
+
+export default function Contact() {
+    return (
+        <div className="text-white">
+            <Navbar/>
+            <h1 className="text-6xl text-center">Contact</h1>
+            <div className="flex justify-between mx-80 py-36">
+                <div>
+                    <div className="h-56 w-[600px] rounded-2xl bg-zinc-800 flex justify-between py-5"></div>
+                    <div className="h-[357px] w-[600px] rounded-2xl bg-zinc-800 flex justify-between my-5"></div>
+                </div>
+                <div className="h-[600px] w-[450px] bg-zinc-800 rounded-2xl">
+                </div>
+                
+            </div>
+            
+            
+            
+        </div>
+    )
+}
