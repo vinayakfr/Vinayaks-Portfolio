ec582e714a974e67c5b486496d3832690695b5dc
vinayakfr <vinayak.sri@yahoo.com>
2024-03-25 00:59:07 +0530

    chore: fix tailwind error in music route

--- app/routes/music.tsx
+++ app/routes/music.tsx
@@ -28,7 +28,7 @@ export default function Music() {
             
             <div className="py-16">
                 <h1 className="text-6xl text-center text-white">My Journey</h1>
-                <h1 className="text-5xl text-center text-white py-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, modi placeat? Nemo ea beatae delectus aspernatur laudantium, velit ipsum perspiciatis! Similique neque perferendis aperiam ipsa. Quasi est expedita officiis cupiditate, ea exercitationem adipisci sit! Doloremque eos voluptas laboriosam quisquam pariatur cupiditate quibusdam, porro non provident sit et quae, id autem?</h1>
+                <h1 className="text-5xl text-center text-white py-10 mx-10 leading-[55px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, modi placeat? Nemo ea beatae delectus aspernatur laudantium, velit ipsum perspiciatis! Similique neque perferendis aperiam ipsa. Quasi est expedita officiis cupiditate, ea exercitationem adipisci sit! Doloremque eos voluptas laboriosam quisquam pariatur cupiditate quibusdam, porro non provident sit et quae, id autem?</h1>
             </div>
             <Footer/>
         </div> 
