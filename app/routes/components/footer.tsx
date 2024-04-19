1d1f824119a3564e478f7a91dd4c17c2fdb93646
vinayakfr <vinayak.sri@yahoo.com>
2024-04-19 14:07:39 +0530

    feat: create projects page

--- app/routes/components/footer.tsx
+++ app/routes/components/footer.tsx
@@ -11,20 +11,20 @@ export default function Footer() {
           <h1 className="font-bold text-center md:text-start text-xl lg:text-2xl 2xl:text-5xl bg-gradient-to-br from-zinc-500 via-rose-800 to-zinc-500 text-transparent bg-clip-text">
             Socials
           </h1>
-          <div className="flex place-content-center place-items-center gap-2 2xl:gap-6 lg:text-xl 2xl:text-3xl">
-            <a href="">GitHub</a>
-            <a href="">LinkedIn</a>
-            <a href="">Instagram</a>
-            <a href="">Twitter</a>
+          <div className="flex place-content-center place-items-center md:place-content-start gap-2 2xl:gap-6 lg:text-xl 2xl:text-3xl">
+            <a href="https://github.com/vinayakfr" className="hover:text-rose-700">GitHub</a>
+            <a href="https://www.linkedin.com/in/vinayaksrivsatava/" className="hover:text-rose-700">LinkedIn</a>
+            <a href="https://www.instagram.com/idkvinayak/" className="hover:text-rose-700">Instagram</a>
+            <a href="" className="hover:text-rose-700">Twitter</a>
           </div>
           <h1 className="font-bold text-center md:text-start text-xl lg:text-2xl 2xl:text-5xl bg-gradient-to-br from-zinc-500 via-rose-800 to-zinc-500 text-transparent bg-clip-text">
             Quick Links
           </h1>
-          <div className="flex place-content-center place-items-center gap-2 2xl:gap-6 lg:text-xl 2xl:text-3xl">
-            <a href="">Projects</a>
-            <a href="">Music</a>
-            <a href="">Playlist</a>
-            <a href="">Contact</a>
+          <div className="flex place-content-center place-items-center md:place-content-start gap-2 2xl:gap-6 lg:text-xl 2xl:text-3xl">
+            <a href="/projects" className="hover:text-rose-700">Projects</a>
+            <a href="" className="hover:text-rose-700">Playlist</a>
+            <a href="/resume" className="hover:text-rose-700">Resume</a>
+            <a href="/contact" className="hover:text-rose-700">Contact</a>
           </div>
         </div>
       </div>
---
+++ app/routes/projects.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+
+function Projects() {
+  return (
+    <div>
+      <Navbar/>
+      <h1 className='text-center xl:text-8xl bg-gradient-to-tr from-zinc-800 to-zinc-800 via-rose-600 text-transparent bg-clip-text'>Projects</h1>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Projects;
