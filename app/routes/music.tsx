1a28010184a93740b40a1de3c31dde766a0b339f
vinayakfr <vinayak.sri@yahoo.com>
2024-06-07 08:04:46 +0530

    chore: repositoin components in music route

--- app/routes/music.tsx
+++ app/routes/music.tsx
@@ -33,72 +33,127 @@ export default function Music() {
           <div className="w-[90%] xl:w-[70%] h-[1px] bg-white my-5"></div>
           <div className="h-[15rem] xl:w-[80%] overflow-auto">
             <h1 className="text-xl xl:text-3xl 2xl:text-5xl text-center text-white mx-10 font-thin 2xl:font-light">
-
-It all started in 3rd grade when Vinayak was given an ultimatum between Dance, Music, and Theatre. With a promise to his mother that he would sing all the songs taught to him in Music, she filled out the music check-box. He wasn't very keen at first, but his interest <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
+              It all started in 3rd grade when Vinayak was given an ultimatum
+              between Dance, Music, and Theatre. With a promise to his mother
+              that he would sing all the songs taught to him in Music, she
+              filled out the music check-box. He wasn't very keen at first, but
+              his interest{" "}
+              <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
                 sparked{" "}
-              </a> when he met his new music teacher in 5th grade. It didn't take much time for the new teacher to become his idol. Vinayak became more  <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
+              </a>{" "}
+              when he met his new music teacher in 5th grade. It didn't take
+              much time for the new teacher to become his idol. Vinayak became
+              more{" "}
+              <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
                 fascinated{" "}
-              </a> by music and wanted to learn everything he could in his music room. Following his old music teacher's suggestion to make him learn the <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
+              </a>{" "}
+              by music and wanted to learn everything he could in his music
+              room. Following his old music teacher's suggestion to make him
+              learn the{" "}
+              <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
                 Tabla{" "}
-              </a> seriously, his mother enrolled him in tabla classes for one year. Although Vinayak wasn't very fond of the tabla, he was quite good at it.
-
-In 6th grade, Vinayak became one of the  <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
+              </a>{" "}
+              seriously, his mother enrolled him in tabla classes for one year.
+              Although Vinayak wasn't very fond of the tabla, he was quite good
+              at it. In 6th grade, Vinayak became one of the{" "}
+              <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
                 founding members{" "}
-              </a> of his school <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
+              </a>{" "}
+              of his school{" "}
+              <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
                 orchestra group{" "}
-              </a>, which was a pretty big deal for him. He loved the stay-backs and the early morning practices, as well as the fact that he could skip class periods for his 'orchestra practice.' He also loved how his music teacher would pick him up in the morning and drop him back at home after practice. The orchestra team always placed in the top three but never scored first place.
-
-In 8th grade, Vinayak started developing an interest in <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
+              </a>
+              , which was a pretty big deal for him. He loved the stay-backs and
+              the early morning practices, as well as the fact that he could
+              skip class periods for his 'orchestra practice.' He also loved how
+              his music teacher would pick him up in the morning and drop him
+              back at home after practice. The orchestra team always placed in
+              the top three but never scored first place. In 8th grade, Vinayak
+              started developing an interest in{" "}
+              <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
                 string instruments
-              </a>, so he joined his music teacher's music school. Vinayak was a 'quick learner,' as his teacher would say; he was indeed <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
+              </a>
+              , so he joined his music teacher's music school. Vinayak was a
+              'quick learner,' as his teacher would say; he was indeed{" "}
+              <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
                 talented
-              </a>. Vinayak took his first music exam in guitar from <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
+              </a>
+              . Vinayak took his first music exam in guitar from{" "}
+              <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
                 Trinity College
-              </a>, London. After changing schools in 11th grade, Vinayak became a part of his new school's orchestra team. Not to his surprise, he was still on <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse"> percussion </a>, as he was before. With his new team, Vinayak returned to his old school, where his journey began, to complete the circle. This time, he won the <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
+              </a>
+              , London. After changing schools in 11th grade, Vinayak became a
+              part of his new school's orchestra team. Not to his surprise, he
+              was still on{" "}
+              <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
+                {" "}
+                percussion{" "}
+              </a>
+              , as he was before. With his new team, Vinayak returned to his old
+              school, where his journey began, to complete the circle. This
+              time, he won the{" "}
+              <a className="bg-gradient-to-tr from-zinc-400 via-rose-600 to-zinc-400 text-transparent bg-clip-text animate-pulse">
                 Zonal Level Orchestra Competition.{" "}
-              </a>. Vinayak felt both happy and nostalgic as he celebrated his victory with his teammates.
+              </a>
+              . Vinayak felt both happy and nostalgic as he celebrated his
+              victory with his teammates.
             </h1>
           </div>
         </div>
       </div>
+      <Timeline />
       <h1 className="text-white text-5xl xl:text-7xl 2xl:text-9xl text-center font-semibold p-10 2xl:p-14">
         And people ask why I always carry headphopnes...👀
       </h1>
-      <div className="flex place-content-center place-items-center px-12">
+      <div className="flex place-content-center place-items-center px-12 pb-12">
         <div className="flex gap-5 overflow-auto">
           <div className="">
             <MusicCard
               image={"artemis.png"}
-              link={"https://music.apple.com/in/album/cant-stop-loving-you/1686132427?i=1686132428"}
-              title={"Can't Stop Loving You - Artemis"} />
+              link={
+                "https://music.apple.com/in/album/cant-stop-loving-you/1686132427?i=1686132428"
+              }
+              title={"Can't Stop Loving You - Artemis"}
+            />
           </div>
           <div>
             <MusicCard
               image={"Stargazing.png"}
-              link={"https://music.apple.com/in/album/stargazing/1541400529?i=1541400545"}
-              title={"Stargazing - The Neighbourhood"} />
+              link={
+                "https://music.apple.com/in/album/stargazing/1541400529?i=1541400545"
+              }
+              title={"Stargazing - The Neighbourhood"}
+            />
           </div>
           <div>
             <MusicCard
               image={"Strawberries.png"}
-              link={"https://music.apple.com/in/album/strawberries-cigarettes/1460746852?i=1460747047"}
-              title={"Strawberries & Cigarretted - Troye Sivan"} />
+              link={
+                "https://music.apple.com/in/album/strawberries-cigarettes/1460746852?i=1460747047"
+              }
+              title={"Strawberries & Cigarretted - Troye Sivan"}
+            />
           </div>
           <div>
             <MusicCard
               image={"Storyofmylife.png"}
-              link={"https://music.apple.com/in/album/story-of-my-life/695312369?i=695312371"}
-              title={"Story of My Life - One Direction"} />
+              link={
+                "https://music.apple.com/in/album/story-of-my-life/695312369?i=695312371"
+              }
+              title={"Story of My Life - One Direction"}
+            />
           </div>
           <div>
             <MusicCard
               image={"Burningroom.png"}
-              link={"https://music.apple.com/in/album/slow-dancing-in-a-burning-room/184335550?i=184335793"}
-              title={"Slow Dancing in a Burning Room - John Mayer"} />
+              link={
+                "https://music.apple.com/in/album/slow-dancing-in-a-burning-room/184335550?i=184335793"
+              }
+              title={"Slow Dancing in a Burning Room - John Mayer"}
+            />
           </div>
         </div>
       </div>
-      <Timeline />
       <Footer />
     </div>
   );
--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,15 +1,52 @@
-import React from 'react'
-import Navbar from '../components/navbar';
-import Footer from '../components/footer';
+import React from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 function Projects() {
   return (
     <div>
-      <Navbar/>
-      <h1 className='text-center xl:text-8xl bg-gradient-to-tr from-zinc-800 to-zinc-800 via-rose-600 text-transparent bg-clip-text'>Projects</h1>
-      <Footer/>
+      <Navbar />
+      <h1 className="text-center xl:text-8xl bg-gradient-to-tr from-zinc-800 to-zinc-800 via-rose-600 text-transparent bg-clip-text">
+        Projects
+      </h1>
+      <div className="text-white flex flex-col gap-16 place-content-center place-items-center py-10">
+        <div className="w-[70%] h-[35rem] rounded-xl outline bg-black flex flex-col place-content-start">
+          <img
+            src=""
+            alt=""
+            className="w-full h-[22.5rem] bg-zinc-800 rounded-xl"
+          />
+          <div className="px-5">
+            <h1 className="text-center text-4xl font-bold py-2">MOZOFEST'23</h1>
+            <p className="font-light text-justify lg:text-lg py-2">
+              Mozofest is the flagship event of SRMKZILLA, the official Mozilla
+              Campus Club at SRM University of Science and Technology, Chennai.
+              This engaging event welcomes participants with all levels of
+              coding skills. It features a variety of games designed to
+              challenge minds and showcase coding proficiency.
+            </p>
+          </div>
+        </div>
+        <div className="w-[70%] h-[35rem] rounded-xl outline bg-black flex flex-col place-content-start">
+          <img
+            src=""
+            alt=""
+            className="w-full h-[22.5rem] bg-zinc-800 rounded-xl"
+          />
+          <div className="px-5">
+            <h1 className="text-center text-4xl font-bold py-2">
+              Unbranded Boutique
+            </h1>
+            <p className="font-light text-justify lg:text-lg py-2">
+              Unbranded Boutique is a manufacturing brand that helps the upcoming skincare brands by manufacturing acne-serums for them and helping them establish their market. They are currently based in USA.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Projects;
